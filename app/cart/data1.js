25f0f8b163a97df70218dd58aae05e1410fc3f6c
LEE YUNGHSIN <lenalee7963@gmail.com>
2023-05-11 16:23:03 +0900

    cart component

---
+++ app/cart/data1.js
@@ -0,0 +1,8 @@
+function insa(){
+    return(
+        <div>
+            <h1>안녕하세요</h1>
+        </div>
+    )
+}
+export default insa;
--- app/cart/page.js
+++ app/cart/page.js
@@ -1,15 +1,19 @@
+'use client'
+
+import 작명 from './data1.js'
+
 export default function Cart() {
   return (
     <div>
       <h4 className="title">Cart</h4>
-      
+      <작명></작명>
       <CartItem/>  
       <CartItem/>
        </div>
   )
 } 
 
-function CartItem(){
+function CartItem(){// 아무데나 대충 만든건 server component
   return(
     <div className="cart-item">
         <p>상품명</p>
@@ -18,3 +22,8 @@ function CartItem(){
       </div>
   )
 }
+
+//'use client' 를 위에 쓰면 client component
+// server에서는 html에 자바스크립트 사용불가. 단 로딩속도가 매우 빠름. 검색엔진 노출 유리
+//client에서는 로딩속도느림, hydration(html을 리액트로 분석하는) 필요. 
+//큰 페이지는 server js가 필요한 곳에만 client component 
