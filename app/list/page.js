7ea70d44684b9e5dcd146af405390cd64a74f653
LEE YUNGHSIN <lenalee7963@gmail.com>
2023-05-17 23:13:14 +0900

    list add function1

--- app/list/page.js
+++ app/list/page.js
@@ -1,7 +1,11 @@
 // import Image from "next/image"
+'use client'
+import { useState } from "react"
+
 // import img from '/public/food0.png'
 export default function List() {
   let 상품 = ['Tomato','Pasta','Coconut']
+  let [like,setLike]=useState([0,0,0]);
       return (
         <div>
             <h4 className="title">상품목록</h4>
@@ -10,7 +14,21 @@ export default function List() {
               return(
               <div className="food" key={i}>
                 <img src={`/food${i}.png`} className="food-img" alt="food"/> 
+                
                 <h4>{a}</h4>
+                <button onClick={()=>{
+                  let copy=[...like];
+                  copy[i]>0?copy[i]=copy[i]-1:null
+                  setLike(copy);
+                  
+                }}>-</button>
+                <span>{like[i]}</span>
+                <button onClick={()=>{
+                  let copy=[...like];
+                  copy[i]=copy[i]+1
+                  setLike(copy);
+                  
+                }}>+</button>
               </div>)
             })}
 
@@ -18,3 +36,4 @@ export default function List() {
       )
     }
     //외부이미지는 width, heigh 속성 필요함 + 외부 파일은 next.config.js에서 도메인 등록 필요. 
+    //onMouse
